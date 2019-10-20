63a3a18536cddc5697e7c6156e7368c95bf7cf16
“Autumn <qiushili@berkeley.edu>
2019-10-20 03:30:02 -0700

    finish

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -19,7 +19,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  #fail "Unimplemented"
+  /#{e1}.*#{e2}/m =~ page.body
 end
 
 # Make it easier to express checking or unchecking several boxes at once
